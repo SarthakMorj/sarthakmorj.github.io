2a8c44eb42805f7c465291f928a425e385f0f26c
Sarthak Kumar Morj <sarthakmorj@Sarthaks-MacBook-Air.local>
2025-07-06 17:29:12 -0400

    Testing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React from 'react'
-import { Element } from 'react-scroll'
-import Navbar        from './components/Navbar'
-import About         from './components/About'
-import Experience    from './components/Experience'
-import Projects      from './components/Projects'
-import Skills        from './components/Skills'
-import Contact       from './components/Contact'
-import DarkModeToggle from './components/DarkModeToggle'
-import './styles/App.css'
+// src/App.js
+
+import React from 'react';
 
 function App() {
   return (
-    <>
-      <DarkModeToggle />
-      <Navbar />
-      <main>
-        <Element name="about"><About /></Element>
-        <Element name="experience"><Experience /></Element>
-        <Element name="projects"><Projects /></Element>
-        <Element name="skills"><Skills /></Element>
-        <Element name="contact"><Contact /></Element>
-      </main>
-    </>
-  )
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Hello, world!</h1>
+      <p>If you see this, your GitHub Pages build is working!</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
